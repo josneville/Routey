5c89bf2475900d16416259ede0def875dc678f31
Neville Jos <josneville@yahoo.com>
2015-12-07 21:26:32 -0600

    most functionality done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,14 @@
 angular.module('starter.controllers', [])
 
+.controller('main', function ($scope) {
+	$scope.options = {
+		location: "201 N Goodwin Ave, Urbana, IL 61801",
+		time: {}
+	}
+	$scope.itineraries = []
+	$scope.itinerary = {}
+})
+
 .controller('search', function ($scope) {
 
 })
@@ -10,63 +19,216 @@ angular.module('starter.controllers', [])
 	}
 })
 
-.controller('search.categories', function ($scope, $state) {
+.controller('search.categories', function ($scope, $state, $filter, MainData) {
+	$scope.categories = angular.copy(MainData.categories)
 	$scope.nextPage = function () {
+		$scope.options.categories = $filter('filter')($scope.categories, {
+			checked: true
+		})
 		$state.go('search.options')
 	}
 })
 
-.controller('search.options', function ($scope, $state, $ionicModal) {
+.controller('search.options', function ($scope, $state, $ionicModal, $filter, MainData) {
 	$ionicModal.fromTemplateUrl('modal.html', function ($ionicModal) {
 		$scope.modal = $ionicModal;
 	}, {
-		// Use our scope for the scope of the modal to keep it simple
 		scope: $scope,
-		// The animation we want to use for the modal entrance
 		animation: 'slide-in-up'
 	});
+	$scope.locations = MainData.locations.filter(function (location) {
+		for (var i = 0; i < $scope.options.categories.length; i++) {
+			if ($scope.options.categories[i].name == location.category) {
+				return true
+			}
+		}
+		return false
+	})
+	$scope.possible_itineraries = combinations($scope.locations)
+	for (var i = 0; i < $scope.possible_itineraries.length; i++) {
+		cost = 0
+		time = 0
+		for (var j = 0; j < $scope.possible_itineraries[i].length; j++) {
+			cost += $scope.possible_itineraries[i][j].cost
+			time += $scope.possible_itineraries[i][j].time.hours * 60 + $scope.possible_itineraries[i][j].time.minutes
+		}
+		if (cost < $scope.options.budget && time < ($scope.options.time.hours * 60 + $scope.options.time.minutes)) {
+			$scope.itineraries.push({
+				locations: $scope.possible_itineraries[i],
+				cost: cost,
+				time: time
+			})
+		}
+	}
+	$scope.itineraries = shuffleArray($scope.itineraries.sort(function (a, b) {
+		return b.time - a.time
+	}).slice(0, 30)).slice(0, 5)
+
+	$scope.selectItinerary = function (itinerary) {
+		$scope.currentItinerary = itinerary
+		$scope.modal.show()
+	}
+
 	$scope.nextPage = function () {
+		$scope.$parent.$parent.itinerary = $scope.currentItinerary
 		$state.go('itinerary')
+	}
+
+	function shuffleArray(array) {
+		for (var i = array.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		return array;
+	}
+
+	function combinations(set) {
+		var k, i, combs, k_combs;
+		combs = [];
+
+		// Calculate all non-empty k-combinations
+		for (k = 1; k <= set.length; k++) {
+			k_combs = k_combinations(set, k);
+			for (i = 0; i < k_combs.length; i++) {
+				combs.push(k_combs[i]);
+			}
+		}
+		return combs;
+	}
+
+	function k_combinations(set, k) {
+		var i, j, combs, head, tailcombs;
+		if (k > set.length || k <= 0) {
+			return [];
+		}
+
+		if (k == set.length) {
+			return [set];
+		}
+
+		if (k == 1) {
+			combs = [];
+			for (i = 0; i < set.length; i++) {
+				combs.push([set[i]]);
+			}
+			return combs;
+		}
+		combs = [];
+		for (i = 0; i < set.length - k + 1; i++) {
+			head = set.slice(i, i + 1);
+			tailcombs = k_combinations(set.slice(i + 1), k - 1);
+			for (j = 0; j < tailcombs.length; j++) {
+				combs.push(head.concat(tailcombs[j]));
+			}
+		}
+		return combs;
 	}
 })
 
 .controller('itinerary', function ($scope, uiGmapGoogleMapApi) {
-	$scope.markers = [{
-		id: 0,
-		coords: {
-			latitude: 40.117091,
-			longitude: -88.242177
-		}
-	}, {
-    id: 1,
-    coords: {
-      latitude: 40.413091,
-      longitude: -87.142177
-    }
-  }, {
-		id: 2,
-		coords: {
-			latitude: 41.113091,
-			longitude: -88.142177
-		}
-	}]
-	var latitude_sum = 0
-	var longitude_sum = 0
-	for (var i = 0; i < $scope.markers; i++) {
-		latitude_sum += $scope.markers.coords.latitude
-		longitude_sum += $scope.markers.coords.longitude
-	}
-	var avg_latitude = latitude_sum / $scope.markers.length
-	var avg_longitude = longitude_sum / $scope.markers.length
-	$scope.map = {
-		center: {
-			latitude: avg_latitude,
-			longitude: avg_longitude
-		},
-		zoom: 4,
-		control: {}
-	}
 	uiGmapGoogleMapApi.then(function (maps) {
+		$scope.markers = $scope.itinerary.locations
+		var waypoints = []
+		for (var i = 0; i < $scope.markers.length; i++) {
+			waypoints.push({
+				location: $scope.markers[i].coords.latitude + ", " + $scope.markers[i].coords.longitude
+			})
+		}
+		$scope.markers.push({
+			id: 100,
+			coords: {
+				latitude: 40.114026,
+				longitude: -88.224807
+			}
+		})
+		$scope.mapOptions = {
+			styles: [{
+				"featureType": "landscape",
+				"stylers": [{
+					"hue": "#FFBB00"
+				}, {
+					"saturation": 43.400000000000006
+				}, {
+					"lightness": 37.599999999999994
+				}, {
+					"gamma": 1
+				}]
+			}, {
+				"featureType": "road.highway",
+				"stylers": [{
+					"hue": "#FFC200"
+				}, {
+					"saturation": -61.8
+				}, {
+					"lightness": 45.599999999999994
+				}, {
+					"gamma": 1
+				}]
+			}, {
+				"featureType": "road.arterial",
+				"stylers": [{
+					"hue": "#FF0300"
+				}, {
+					"saturation": -100
+				}, {
+					"lightness": 51.19999999999999
+				}, {
+					"gamma": 1
+				}]
+			}, {
+				"featureType": "road.local",
+				"stylers": [{
+					"hue": "#FF0300"
+				}, {
+					"saturation": -100
+				}, {
+					"lightness": 52
+				}, {
+					"gamma": 1
+				}]
+			}, {
+				"featureType": "water",
+				"stylers": [{
+					"hue": "#0078FF"
+				}, {
+					"saturation": -13.200000000000003
+				}, {
+					"lightness": 2.4000000000000057
+				}, {
+					"gamma": 1
+				}]
+			}, {
+				"featureType": "poi",
+				"stylers": [{
+					"hue": "#00FF6A"
+				}, {
+					"saturation": -1.0989010989011234
+				}, {
+					"lightness": 11.200000000000017
+				}, {
+					"gamma": 1
+				}]
+			}]
+		}
+
+		var latitude_sum = 0
+		var longitude_sum = 0
+		for (var i = 0; i < $scope.markers.length; i++) {
+			latitude_sum += $scope.markers[i].coords.latitude
+			longitude_sum += $scope.markers[i].coords.longitude
+		}
+		var avg_latitude = latitude_sum / $scope.markers.length
+		var avg_longitude = longitude_sum / $scope.markers.length
+		$scope.map = {
+			center: {
+				latitude: avg_latitude,
+				longitude: avg_longitude
+			},
+			zoom: 6,
+			control: {}
+		}
 		var directionsDisplay = new maps.DirectionsRenderer({
 			suppressMarkers: true
 		});
@@ -75,13 +237,8 @@ angular.module('starter.controllers', [])
 
 		// directions object -- with defaults
 		$scope.directions = {
-			origin: $scope.markers[0].coords.latitude + "," + $scope.markers[0].coords.longitude,
-			waypoints: [{
-				location: $scope.markers[1].coords.latitude + "," + $scope.markers[1].coords.longitude
-			},
-      {
-        location: $scope.markers[2].coords.latitude + "," + $scope.markers[2].coords.longitude
-      }],
+			origin: $scope.options.location,
+			waypoints: waypoints,
 			showList: false
 		}
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,237 @@
 angular.module('starter.services', [])
+
+.factory('MainData', function () {
+	return {
+		locations: [{
+			id: 0,
+			name: "Krannert Center for the Performance Arts",
+			coords: {
+				latitude: 40.108203,
+				longitude: -88.222443
+			},
+			category: "Arts & Entertainment",
+			image: "img/krannert.jpg",
+			options: {
+				icon: {
+					url: "img/sm/krannert.jpg"
+				}
+			},
+			cost: 20,
+			time: {
+				hours: 2,
+				minutes: 15
+			}
+		}, {
+			id: 1,
+			name: "Urbana Indoor Aquatic Center",
+			coords: {
+				latitude: 40.103488,
+				longitude: -88.206898
+			},
+			category: "Active Life",
+			image: "img/aquatic.jpg",
+			options: {
+				icon: {
+					url: "img/sm/aquatic.jpg"
+				}
+			},
+			cost: 10,
+			time: {
+				hours: 1,
+				minutes: 30
+			}
+		}, {
+			id: 2,
+			name: "Urbana Dog Park",
+			coords: {
+				latitude: 40.126868,
+				longitude: -88.188583
+			},
+			category: "Parks",
+			image: "img/dog_park.jpeg",
+			options: {
+				icon: {
+					url: "img/sm/dog_park.jpg"
+				}
+			},
+			cost: 0,
+			time: {
+				hours: 1,
+				minutes: 15
+			}
+		}, {
+			id: 3,
+			name: "Market Place Shopping Center",
+			coords: {
+				latitude: 40.142708,
+				longitude: -88.243130
+			},
+			category: "Shopping",
+			image: "img/marketplace.jpg",
+			options: {
+				icon: {
+					url: "img/sm/marketplace.jpg"
+				}
+			},
+			cost: 100,
+			time: {
+				hours: 2,
+				minutes: 0
+			}
+		}, {
+			id: 4,
+			name: "The Body Therapy Center",
+			coords: {
+				latitude: 40.118187,
+				longitude: -88.244851
+			},
+			category: "Beauty & Spas",
+			image: "img/body_therapy.jpeg",
+			options: {
+				icon: {
+					url: "img/sm/body_therapy.jpg"
+				}
+			},
+			cost: 50,
+			time: {
+				hours: 0,
+				minutes: 45
+			}
+		}, {
+			id: 5,
+			name: "The Blind Pig Bar",
+			coords: {
+				latitude: 40.117091,
+				longitude: -88.242177
+			},
+			category: "Nightlife",
+			image: "img/blind_pig.jpg",
+			options: {
+				icon: {
+					url: "img/sm/blind_pig.jpg"
+				}
+			},
+			cost: 20,
+			time: {
+				hours: 0,
+				minutes: 50
+			}
+		}, {
+			id: 6,
+			name: "Golden Harbor Authentic Chinese Cuisine",
+			coords: {
+				latitude: 40.111354,
+				longitude: -88.243753
+			},
+			category: "Restaurant",
+			image: "img/golden-harbor.jpg",
+			options: {
+				icon: {
+					url: "img/sm/golden-harbor.jpg"
+				}
+			},
+			cost: 60,
+			time: {
+				hours: 1,
+				minutes: 30
+			}
+		}, {
+			id: 7,
+			name: "Hampton Inn Champaign/Urbana",
+			coords: {
+				latitude: 40.117213,
+				longitude: -88.224870
+			},
+			category: "Hotels & Travel",
+			image: "img/hampton.jpg",
+			options: {
+				icon: {
+					url: "img/sm/hampton.jpg"
+				}
+			},
+			cost: 110,
+			time: {
+				hours: 8,
+				minutes: 30
+			}
+		}, {
+			id: 8,
+			name: "Murphy's Pub",
+			coords: {
+				latitude: 40.110744,
+				longitude: -88.230146
+			},
+			category: "Nightlife",
+			image: "img/murphy.jpg",
+			options: {
+				icon: {
+					url: "img/sm/murphy.jpg"
+				}
+			},
+			cost: 30,
+			time: {
+				hours: 1,
+				minutes: 45
+			}
+		}, {
+			id: 9,
+			name: "Sitara Indian Restaurant & Lounge",
+			coords: {
+				latitude: 40.112164,
+				longitude: -88.208609,
+			},
+			category: "Restaurants",
+			image: "img/sitara.jpg",
+			options: {
+				icon: {
+					url: "img/sm/sitara.jpg"
+				}
+			},
+			cost: 40,
+			time: {
+				hours: 1,
+				minutes: 0
+			}
+		}],
+		categories: [{
+			name: "Food",
+			checked: true
+		}, {
+			name: "Nightlife",
+			checked: true
+		}, {
+			name: "Restaurants",
+			checked: true
+		}, {
+			name: "Shopping",
+			checked: true
+		}, {
+			name: "Active Life",
+			checked: true
+		}, {
+			name: "Arts & Entertainment",
+			checked: true
+		}, {
+			name: "Beauty & Spas",
+			checked: true
+		}, {
+			name: "Education",
+			checked: true
+		}, {
+			name: "Event Planning & Services",
+			checked: true
+		}, {
+			name: "Health & Medical",
+			checked: true
+		}, {
+			name: "Hotels & Travel",
+			checked: true
+		}, {
+			name: "Museums",
+			checked: true
+		}, {
+			name: "Parks",
+			checked: true
+		}, ]
+	}
+})
